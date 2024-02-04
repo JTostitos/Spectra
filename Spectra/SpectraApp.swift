c41bd9b1ac6bbec64cc1cb8eaca9107580d23a56
JTostitos <17661536+JTostitos@users.noreply.github.com>
2024-02-03 23:31:27 -0500

    Initial Commit

---
+++ Spectra/SpectraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpectraApp.swift
+//  Spectra
+//
+//  Created by Jonathan Tsistinas on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SpectraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
